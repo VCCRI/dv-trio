ac49c8e9f862ee59ce436792439c9387844a2188
Eddie Ip <33052080+eipVCCRI@users.noreply.github.com>
2019-06-05 01:38:04 +1000

    Add files via upload

--- dv-trio_famseq_test.sh
+++ dv-trio_famseq_test.sh
@@ -39,7 +39,7 @@ for i in {1..18} # check for 3 hrs max
 do 
  find $TEMP_DIR -name $FAMSEQ_MOD_TEMPSPLIT_pref"*_done.txt" > $FAMSEQ_MOD_TEMPO3 
  nfile=$(wc -l $FAMSEQ_MOD_TEMPO3)
- if [ $nfile == $sfile ];
+ if [ $nfile = $sfile ];
  then
 	famseq_complete=true
 	break
