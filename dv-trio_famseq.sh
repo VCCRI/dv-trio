012fa4adc6cd28af7415ce387410a7ff8f93f723
Eddie Ip <33052080+eipVCCRI@users.noreply.github.com>
2019-06-05 11:07:01 +1000

    Add files via upload

--- dv-trio_famseq.sh
+++ dv-trio_famseq.sh
@@ -56,10 +56,62 @@ sed "s|\.\/\.\:0\,0\:\.\:\.\:\.\:\.|\.\/\.\:0\,0\:0\:\.\:0\,0\,0\:\.|g" $TEMP_DI
 #
 echo "Running FamSeq..."
 FAMSEQ_OUTPUT="${FAMSEQ_DIR}/trio.FamSeq.vcf"
+FAMSEQ_MOD_OUTPUT="${FAMSEQ_DIR}/trio.FamSeq_mod.vcf"
 FamSeq vcf -vcfFile $TEMP_DIR/famseq_input.dec.bs.processed.corr.vcf -pedFile $ped -output ${FAMSEQ_OUTPUT} -a -LRC ${threshold}
 sed -i 's/[ \t]*$//' $FAMSEQ_OUTPUT #
 sed -i 's|PP\,Number\=G\,Type\=Integer|PP\,Number\=G\,Type\=Float|g' $FAMSEQ_OUTPUT #
-###################
-echo -e "OUT\t$FAMSEQ_OUTPUT" > $FAMSEQ_DIR/famseq_done.txt
-echo "$(date) - FamSeq completed"
-
+#
+#save original GT value and move Famseq GT to be the GT value for sample
+bcftools view -h $FAMSEQ_OUTPUT > $FAMSEQ_MOD_OUTPUT # firstly change the header
+sed -i 's|ID=GT\,|ID=OGT\,|g' $FAMSEQ_MOD_OUTPUT #
+sed -i 's|ID=FGT\,|ID=GT\,|g' $FAMSEQ_MOD_OUTPUT #
+numb_col=$(tail -n1 $FAMSEQ_MOD_OUTPUT | wc -w)
+nsamp=$(($nbr_col-9))
+#
+FAMSEQ_MOD_TEMPO1="$TEMP_DIR/vcf_famseq_mod_temp.txt"
+FAMSEQ_MOD_TEMPO2="$TEMP_DIR/vcf_famseq_mod_split_list1.txt"
+FAMSEQ_MOD_TEMPO3="$TEMP_DIR/vcf_famseq_mod_split_list2.txt"
+FAMSEQ_MOD_TEMPVCF="$TEMP_DIR/vcf_famseq_mod_temp.vcf"
+FAMSEQ_MOD_TEMPSPLIT_pref="vcf_famseq_mod_splittemp_"
+FAMSEQ_MOD_TEMPSPLIT="$TEMP_DIR/$FAMSEQ_MOD_TEMPSPLIT_pref"
+bcftools view -H $FAMSEQ_OUTPUT | grep "FGT" > $FAMSEQ_MOD_TEMPO1
+bcftools view -H $FAMSEQ_OUTPUT | grep -v "FGT" >> $FAMSEQ_MOD_TEMPVCF # put all the variants that were not changed by FamSeq to new output VCF - sort it later
+split -n 23 -d $FAMSEQ_MOD_TEMPO1 $FAMSEQ_MOD_TEMPSPLIT  # split up the variants that were change by FamSeq
+find $TEMP_DIR -name $FAMSEQ_MOD_TEMPSPLIT_pref"*" > $FAMSEQ_MOD_TEMPO2 #
+sfile=$(cat $FAMSEQ_MOD_TEMPO2 | wc -l) # get number of splits
+#
+while read line;     # do while there are lines from input file
+do #
+	bash dv-trio_famseq_finalise.sh $line &
+done < $FAMSEQ_MOD_TEMPO2  #
+#
+famseq_complete=false
+for i in {1..18} # check for 3 hrs max
+do 
+ find $TEMP_DIR -name $FAMSEQ_MOD_TEMPSPLIT_pref"*.done" > $FAMSEQ_MOD_TEMPO3 
+ nfile=$(cat $FAMSEQ_MOD_TEMPO3 | wc -l)
+ if [[ $nfile = $sfile ]];
+ then
+	famseq_complete=true
+	break
+ else
+	sleep 10m #
+ fi
+done
+# check if conversion completed
+if [ "$famseq_complete" = true ]; #
+ then #
+	find $TEMP_DIR -name $FAMSEQ_MOD_TEMPSPLIT_pref"*.txt" > $FAMSEQ_MOD_TEMPO3 
+	while read line;     # do while there are lines from input file
+	do #
+		cat $line >> $FAMSEQ_MOD_TEMPVCF #
+	done < $FAMSEQ_MOD_TEMPO3  #
+#
+	bcftools sort -o $FAMSEQ_MOD_OUTPUT -O v -T $TEMP_DIR $FAMSEQ_MOD_TEMPVCF #
+##################
+	echo -e "OUT\t$FAMSEQ_OUTPUT" > $FAMSEQ_DIR/famseq_done.txt
+	echo "$(date) - FamSeq completed"
+ else #
+	echo "$(date) - FamSeq not completed"
+fi #
+#
